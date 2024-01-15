a726afa7ea5d6ebb6e28fc4c3e6d788c465685f0
Aleksander <olekston@gmail.com>
2024-01-15 16:44:16 +0100

    login basic fragment view

--- app/src/main/java/com/example/booksharing/ui/library/LibraryFragment.kt
+++ app/src/main/java/com/example/booksharing/ui/library/LibraryFragment.kt
@@ -7,11 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.example.booksharing.databinding.FragmentGalleryBinding
+import com.example.booksharing.databinding.FragmentLibraryBinding
 
 class LibraryFragment : Fragment() {
 
-    private var _binding: FragmentGalleryBinding? = null
+    private var _binding: FragmentLibraryBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -25,7 +25,7 @@ class LibraryFragment : Fragment() {
         val libraryViewModel =
             ViewModelProvider(this).get(LibraryViewModel::class.java)
 
-        _binding = FragmentGalleryBinding.inflate(inflater, container, false)
+        _binding = FragmentLibraryBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
         val textView: TextView = binding.textGallery
--- app/src/main/java/com/example/booksharing/ui/login/LoginFragment.kt
+++ app/src/main/java/com/example/booksharing/ui/login/LoginFragment.kt
@@ -7,11 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.example.booksharing.databinding.FragmentHomeBinding
+import com.example.booksharing.databinding.FragmentLoginBinding
 
 class LoginFragment : Fragment() {
 
-    private var _binding: FragmentHomeBinding? = null
+    private var _binding: FragmentLoginBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -25,7 +25,7 @@ class LoginFragment : Fragment() {
         val loginViewModel =
             ViewModelProvider(this).get(LoginViewModel::class.java)
 
-        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        _binding = FragmentLoginBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
         val textView: TextView = binding.textHome
