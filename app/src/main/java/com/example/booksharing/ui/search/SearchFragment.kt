1d2ad545c007267b1828c6f9818b6fa02657f771
Aleksander <olekston@gmail.com>
2024-01-21 14:29:16 +0100

    21.01 added location to the account commit

--- app/src/main/java/com/example/booksharing/ui/search/SearchFragment.kt
+++ app/src/main/java/com/example/booksharing/ui/search/SearchFragment.kt
@@ -4,21 +4,47 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.ListView
+import android.widget.Spinner
 import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
 import com.example.booksharing.R
-import com.example.booksharing.databinding.FragmentSearchBinding
 
 class SearchFragment : Fragment() {
+    private lateinit var textViewChose:TextView
+    private lateinit var spinnerGenre: Spinner
+    private lateinit var buttonSearch: Button
+    private lateinit var listViewBooks: ListView
+    private lateinit var listAdapter: ArrayAdapter<String>
+    private var selectedGenre: String? = null
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-
         val view = inflater.inflate(R.layout.fragment_search,container,false)
+        textViewChose = view.findViewById(R.id.textViewChose)
+        buttonSearch = view.findViewById(R.id.buttonSearch)
+        listViewBooks = view.findViewById(R.id.listViewBooks)
+        listAdapter = ArrayAdapter(this.requireContext(), R.layout.find_books_item, mutableListOf())
+        listViewBooks.adapter = listAdapter
 
+        spinnerGenre = view.findViewById(R.id.spinnerGenre)
+        val genres = arrayOf("Fantasy", "Science Fiction", "Romance", "Mystery", "Thriller", "Non-fiction")
+        val adapter = ArrayAdapter(this.requireContext(), android.R.layout.simple_spinner_dropdown_item, genres)
+        spinnerGenre.adapter = adapter
+
+        spinnerGenre.setOnItemSelectedListener(object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                selectedGenre = genres[position]
+
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {}
+        })
         return view
     }
 }
